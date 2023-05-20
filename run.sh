d5e2095fa4ab11f4172bf5c3673c992bc2983df2
zacky <132923924+zacky46@users.noreply.github.com>
2023-05-21 06:02:57 +0700

    kernel-script: remove some config

--- run.sh
+++ run.sh
@@ -106,7 +106,6 @@ cp -r "$KERNEL_IMG" "$AK3_DIR"/kernel/
 cd "$AK3_DIR" || exit
 zip -r9 "$ZIP_NAME" ./*
 cd "$KERNEL_DIR" || exit
-cp "$AK3_DIR"/*.zip kernel-done/
 
 # End count and calculate total build time
 BUILD_END=$(date +"%s")
